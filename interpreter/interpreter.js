f14aace96494157f347a0755efb709e53ca09017
Matthew Sotoudeh <matthewsot@outlook.com>
2017-07-25 13:56:26 -0700

    commented the interpreter

--- interpreter/interpreter.js
+++ interpreter/interpreter.js
@@ -3,14 +3,15 @@ function Yaepl(opts) {
     this.options.logHandle = this.options.outHandle || console.log;
     this.options.promptHandle = this.options.promptHandle || function (a, callback) { callback(window.prompt(a)); };
     this.flags = {
-        inFunc: false
+        inOp: false
     };
     this.scopes = [ {} ];
     this.currScopeIndex = 0;
     this.currFunc = null;
     this.fullText = [];
 }
-Yaepl.prototype.callbackOps = [ "prompt-str" ];
+
+//These are ops defined on the global scope - you can call them from anywhere
 Yaepl.prototype.globalScope = {
     "add": function (a, b) { return a + b; },
     "subtract": function (a, b) { return a - b; },
@@ -40,7 +41,7 @@ Yaepl.prototype.globalScope = {
     "and": function (a, b) { return (a && b); },
     "jump-bwd": function (b) {
         for (var i = b.targetIndex; i < this.fullText.length; i++) {
-            this.interpretLine(this.fullText[i], false);
+            this.interpretLine(this.fullText[i], function () {}, false);
         }
     },
     "jump-bwd-if": function (a, b) {
@@ -68,35 +69,40 @@ Yaepl.prototype.globalScope = {
         this.globalScope["jump"].call(this, l);
     }
 };
+
+//These are the ops that use callbacks
+Yaepl.prototype.callbackOps = [ "prompt-str" ];
+
+//Splits a YAEPL parameter string into a list of different
+//string YAPEL parameters. Does NOT evaluate them - they're
+//all strings
 Yaepl.prototype.splitParams = function (params) {
-    var split_params = params.split(" ");
     var split_params = [];
+
     var curr_param = "";
-    var flags = { dbl_quotes: false, single_quotes: false, escaped: false };
+    var flags = { quote: null, escaped: false };
     for (var i = 0; i < params.length; i++) {
         var chr = params[i];
         curr_param += chr;
+
         if (flags.escaped) {
             flags.escaped = false;
             continue;
         }
+
         flags.escaped = (chr == "\\");
         if (flags.escaped) {
             curr_param = curr_param.substring(0, curr_param.length - 1);
         }
-        else if ((flags.dbl_quotes && chr == '"') || (flags.single_quotes && chr == "'")) {
-            flags.dbl_quotes = false;
-            flags.single_quotes = false;
+        else if (chr === flags.quote) {
+            flags.quote = null;
             split_params.push(curr_param);
             curr_param = "";
         }
-        else if (!flags.single_quotes && chr == '"') {
-            flags.dbl_quotes = true;
+        else if (chr == "\"" || chr == "'") {
+            flags.quote = chr;
         }
-        else if (!flags.dbl_quotes && chr == "'") {
-            flags.single_quotes = true;
-        }
-        else if (!flags.single_quotes && !flags.dbl_quotes && chr == " ") {
+        else if (flags.quote === null && chr == " ") {
             split_params.push(curr_param.trim());
             curr_param = "";
         }
@@ -106,40 +112,69 @@ Yaepl.prototype.splitParams = function (params) {
     }
     return split_params;
 };
-Yaepl.prototype.interpretLine = function (line, callback, addToHistory) {
-    line = line.replace(/\s/, " ");
-    if (addToHistory !== false) this.fullText.push(line);
-    var startComment = line.indexOf("//");
-    if (startComment > -1) {
-        line = line.substring(0, startComment);
-    }
-    line = line.trim();
-    if (line.startsWith("@") && line.indexOf("@end") == -1) {
-        this.flags.inFunc = true;
-        var name = line.split(" ")[0];
-        var params = line.split(name)[1].trim().split(":")[0];
-        if (params != "") {
-            params = params.split(" ");
-        } else {
-            params = [ ];
+//Converts parameter strings to their actual values
+Yaepl.prototype.evaluateParams = function (op, line, params) {
+    //The final param for a jump op is the label, we store that as a string
+    //since it might not be in the scope yet (if it's a jump-fwd)
+    var isJump = [ "jump-fwd", "jump-fwd-if", "jump", "jump-if" ].indexOf(op) > -1;
+    var end = isJump ? 0 : 1;
+
+    for (var p = 0; p < params.length - end; p++) {
+        if (params[p].startsWith("'") || params[p].startsWith("\"")) {
+            params[p] = eval(params[p]);
         }
+        else {
+            params[p] = this.scopes[this.currScopeIndex][params[p]];
+        }
+    }
+    return params;
+};
+
+//Reads a single line and completely interprets it. Note that this
+//may involved the evaluation of previous lines, if it includes a
+//jump operation or a custom operation.
+Yaepl.prototype.interpretLine = function (line, callback, addToHistory) {
+    line = line.replace(/\s/, " ").trim();
+    if (addToHistory !== false) {
+        this.fullText.push(line);
+    }
+
+    if (line.indexOf("//") > -1) {
+        line = line.substring(0, line.indexOf("//"));
+    }
+
+    //Handle the start of custom ops
+    if (line.startsWith("@") && line.indexOf("@end") == -1) {
+        this.flags.inOp = true;
+        var name = line.split(" ")[0];
+
+        //Break up the parameters of the functions
+        //into a list
+        var params = line.substring(name.length).trim().split(":")[0];
+        params = (params == "") ? [] : params.split(" ");
+        
         this.currFunc = {
             name: name.substring(1),
             params: params,
             contents: [ ],
             type: "function"
         };
+
         return callback();
     }
-    if (this.flags.inFunc && line.indexOf("@end") > -1) {
-        this.flags.inFunc = false;
-        this.scopes[this.currScopeIndex][this.currFunc.name] = this.currFunc;
-        return callback();
-    }
-    if (this.flags.inFunc) {
+    //Handles the insides of custom ops
+    if (this.flags.inOp) {
         this.currFunc.contents.push(line);
         return callback();
     }
+    //Handles the end of custom ops
+    if (this.flags.inOp && line.indexOf("@end") > -1) {
+        this.flags.inOp = false;
+        this.scopes[this.currScopeIndex][this.currFunc.name] = this.currFunc;
+        return callback();
+    }
+
+    //Handle code labels
     if (line.startsWith("#")) {
         this.scopes[this.currScopeIndex][line.split(":")[0]] = {
             name: line.split(":")[0],
@@ -152,28 +187,20 @@ Yaepl.prototype.interpretLine = function (line, callback, addToHistory) {
         }
         return callback();
     }
+    //If we're waiting to reach a label don't do anything
     if (this.flags.jumpingFwd) {
         return callback();
     }
+
+    //Now we deal with actual lines that evaluate
     var op = line.split(" ")[0];
     var params = this.splitParams(line.substring(op.length).split("->")[0].trim());
-    for (var p = 0; p < params.length; p++) {
-        if (params[p].startsWith("'") || params[p].startsWith("\"")) {
-            params[p] = eval(params[p]);
-        }
-        else {
-            params[p] = this.scopes[this.currScopeIndex][params[p]];
-        }
-    }
-    if ([ "jump-fwd", "jump-fwd-if", "jump", "jump-if" ].indexOf(op) > -1) {
-        params[params.length - 1] = this.splitParams(line.substring(op.length).split("->")[0].trim())[params.length - 1];
-    }
+    params = this.evaluateParams(op, line, params);
+
     var storeIn = line.split("->");
-    if (storeIn.length > 1) {
-        storeIn = storeIn[1].trim();
-    } else {
-        storeIn = null;
-    }
+    storeIn = (storeIn.length > 1) ? storeIn[1].trim() : null;
+
+    //Global-scope operations
     if (this.globalScope[op] != undefined && this.callbackOps.indexOf(op) == -1) {
         var ret = this.globalScope[op].apply(this, params);
         if (storeIn != null) {
@@ -181,6 +208,7 @@ Yaepl.prototype.interpretLine = function (line, callback, addToHistory) {
         }
         return callback();
     }
+    //Global-scope callback operations
     if (this.globalScope[op] != undefined && this.callbackOps.indexOf(op) != -1) {
         params.push(function (callback, ret) {
             if (storeIn != null) {
@@ -191,31 +219,36 @@ Yaepl.prototype.interpretLine = function (line, callback, addToHistory) {
         var ret = this.globalScope[op].apply(this, params);
         return;
     }
+    //Custom ops
     if (this.scopes[this.currScopeIndex][op] != undefined) {
         var item = this.scopes[this.currScopeIndex][op];
         var prevScopeIndex = this.currScopeIndex;
+
+        //Build a new scope for the function to execute in
+        //and add the arguments
         var newScope = {};
         for (var i = 0; i < item.params.length; i++) {
-            if (i < params.length) {
-                newScope[item.params[i]] = params[i];
-            } else { //Technically we could just do params.length and ignore this if (as they'd be undefined by default), but that feels wrong
-                newScope[item.params[i]] = undefined;
-            }
+            newScope[item.params[i]] = (i < params.length) ? params[i] : undefined;
         }
-        //In another scope
+
+        //In another scope, run the function line-by-line
         this.scopes.push(newScope);
         this.currScopeIndex = this.scopes.length - 1;
         var ret = null;
         for (var i = 0; i < item.contents.length; i++) {
             ret = this.interpretLine(item.contents[i]);
         }
+
+        //Now remove the old scope
         this.scopes.splice(this.currScopeIndex, 1);
         this.currScopeIndex = prevScopeIndex;
+
         //Back to our scope
         if (storeIn != null) {
             this.scopes[this.currScopeIndex][storeIn] = ret;
         }
     }
+    //Return op
     if (op == "return") {
         callback(params[0]);
         return params[0];
